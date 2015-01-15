375907a7035fbde83f94333af064a5bc3c4c87a9
Jônatas Paganini <jonatasdp@gmail.com>
2015-01-15 14:13:51 -0200

    describing the first behaviour.

---
+++ spec/rd-salesforce_spec.rb
@@ -0,0 +1,16 @@
+describe Rd::Salesforce do
+  describe "client" do
+    describe "configuration" do
+      it "allow multiple accounts"
+      it "validates input params"
+    end
+    describe "uploads" do
+      it "new person"
+      it "only valid records"
+    end
+  end
+  describe "person" do
+    it "have basic attributes"
+    it "validate fields"
+  end
+end
